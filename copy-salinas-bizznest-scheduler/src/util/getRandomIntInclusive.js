583626e2556877f2dee4d647407c791f6c432f04
Acab <60949349+AlonzoCabreraS@users.noreply.github.com>
2024-09-27 12:20:37 -0700

    Delete copy-salinas-bizznest-scheduler directory

--- copy-salinas-bizznest-scheduler/src/util/getRandomIntInclusive.js
+++
@@ -1,3 +0,0 @@
-export default function getRandomIntExclusive(a, b) {
-  return Math.floor(Math.random() * (b - a)) + a;
-}
--- copy-salinas-bizznest-scheduler/src/util/shuffle.js
+++
@@ -1,9 +0,0 @@
-import swap from "./swap.js";
-import getRandomIntInclusive from "./getRandomIntInclusive.js";
-
-export default function shuffle(arr) {
-  for (let i = arr.length - 1; i > 0; --i) {
-    const randomIndex = getRandomIntInclusive(0, i);
-    swap(arr, randomIndex, i);
-  }
-}
